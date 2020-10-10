2b32860c7fc6b2567dcd1afe5e1e6bfb4b1dafbc
pkpsyder007 <pkpsyder007@gmail.com>
2020-10-10 13:50:38 +0530

    Updates

---
+++ precache-manifest.f338ab4f03b35d37f3570c3b616a4796.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba35a47c8d730dc9e468c474d6d5198",
+    "url": "/oss-iiitu/index.html"
+  },
+  {
+    "revision": "77166b8c02e08df125e5",
+    "url": "/oss-iiitu/static/css/main.251058d5.chunk.css"
+  },
+  {
+    "revision": "60c52d82869a38d76ae4",
+    "url": "/oss-iiitu/static/js/2.a397dc1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oss-iiitu/static/js/2.a397dc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77166b8c02e08df125e5",
+    "url": "/oss-iiitu/static/js/main.d1f97b3b.chunk.js"
+  },
+  {
+    "revision": "419a76daa6d1d1382a26",
+    "url": "/oss-iiitu/static/js/runtime-main.3d6eaa95.js"
+  },
+  {
+    "revision": "d4624ffe2f81fcd7bb2a1efc304ced9c",
+    "url": "/oss-iiitu/static/media/community.d4624ffe.svg"
+  },
+  {
+    "revision": "bcb3c1e950fb0552b86eedd052f5f309",
+    "url": "/oss-iiitu/static/media/event-1.1.bcb3c1e9.jpg"
+  },
+  {
+    "revision": "981ecf144f1a4b9324c1afca37ecd422",
+    "url": "/oss-iiitu/static/media/head.981ecf14.jpg"
+  },
+  {
+    "revision": "2114cea1b5a43f42f8c075fb70f06ae9",
+    "url": "/oss-iiitu/static/media/info-1.2114cea1.svg"
+  },
+  {
+    "revision": "f5774c5eb8d4fa3136dd9f353fa7cfce",
+    "url": "/oss-iiitu/static/media/info-3.f5774c5e.svg"
+  }
+]);
